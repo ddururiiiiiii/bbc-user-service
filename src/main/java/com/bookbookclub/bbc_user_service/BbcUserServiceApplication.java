6665cb78119ab85e09a6ecba7bcfa8a968128e24
ddururiiiiiii <o244227@daum.net>
2025-06-14 00:10:58 +0900

    chore(config): user/post 서비스의 application.yml 이름을 명확하게 변경
    - user-service → application-user.yml
    - post-service → application-post.yml
    - SpringApplicationBuilder를 통해 각 서비스에서 config name 지정

--- src/main/java/com/bookbookclub/bbc_user_service/BbcUserServiceApplication.java
+++ src/main/java/com/bookbookclub/bbc_user_service/BbcUserServiceApplication.java
@@ -2,12 +2,13 @@ package com.bookbookclub.bbc_user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BbcUserServiceApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(BbcUserServiceApplication.class, args);
+		new SpringApplicationBuilder(BbcUserServiceApplication.class)
+				.properties("spring.config.name=application-user")
+				.run(args);
 	}
-
 }
